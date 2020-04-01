819071efadfa9772e631e8f194cd88815bd9284c
Chris Beynon <chris@chrisbeynon.com>
2020-04-01 17:38:03 +0100

    Updated to only draw "occupied" squares on the grid.

--- diseaseSimV2.js
+++ diseaseSimV2.js
@@ -2,9 +2,11 @@
 
 //----------KEY----------//
 //Numbers used to save space. (possibly speed up calculation as no converting and shorter read write?)
-// 0 = empty
-// 1 = uninfected
-// 2 = infected
+// 0 = empty - no color
+// 1 = uninfected - blue
+// 2 = infected - red
+// 3 = recovered - green
+// 4 = dead - black
 
 
 //----------Page setup---------//
@@ -13,7 +15,7 @@
 var pageCanvas = document.querySelector('canvas');
 
 //Setting the canvas size.Currently a cube, may change later.
-var pageCanvasSize = 500;
+var pageCanvasSize = 1000;
 
 //Setting the canvas size on the page.
 canvas.width = pageCanvasSize;
@@ -24,7 +26,7 @@ canvas.height = pageCanvasSize;
 
 //Setting the grid size that will be used for the simulation.
 //Means that the width/height of the canvas will be divided into 10 sections.
-var gridSize = 10;
+var gridSize = 50;
 
 //Initalises the Array used to store the grid.
 //This will be a 2d array to store rows and columns.
@@ -40,11 +42,11 @@ var ctx = pageCanvas.getContext('2d');
 
 //Variable used to position the squares correctly in the Canvas.
 //+1 used to add a border to the squares. Works alonside the -2 squareSize.
-var positionMultiplier = ((pageCanvasSize / gridSize) + 1);
+var positionMultiplier = (pageCanvasSize / gridSize);
 
 //Variable used to size the grid squares.
 //-2 used to add a border to the squares. Works alonside the +1 above.
-var squareSize = ((pageCanvasSize / gridSize) - 2);
+var squareSize = ((pageCanvasSize / gridSize) - 1);
 
 
 //---------Initial setup of simulation----------//
@@ -74,7 +76,11 @@ for (var i = 0; i < peopleCount; i++) {
 for (var i = 0; i < gridSize; i++) {
   for (var x = 0; x < gridSize; x++) {
     console.log(gridArray[i][x]);
-    ctx.fillRect(i * positionMultiplier, x * positionMultiplier, squareSize, squareSize);
+
+    //ctx.fillRect(i * positionMultiplier, x * positionMultiplier, squareSize, squareSize);
+    if (gridArray[i][x] == 1) {
+      draw(i * positionMultiplier, x * positionMultiplier, squareSize, squareSize, "blue");
+    }
   };
 };
 
@@ -82,10 +88,30 @@ for (var i = 0; i < gridSize; i++) {
 
 //----------Simulation Functions----------//
 
+function draw(x,y,width,height,color) {
+
+  if (color == "blue") {
+    ctx.fillStyle = 'rgb(0, 0, 250)';
+    ctx.fillRect(x, y, width, height);
+  }
+  if (color == "red") {
+    ctx.fillStyle = 'rgb(250, 0, 0)';
+    ctx.fillRect(x, y, width, height);
+  }
+  if (color == "green") {
+    ctx.fillStyle = 'rgb(0, 250, 0)';
+    ctx.fillRect(x, y, width, height);
+  }
+  if (color == "black") {
+    ctx.fillStyle = 'rgb(250, 250, 250)';
+    ctx.fillRect(x, y, width, height);
+  }
+}
 
 
-
-
+function peopleMove() {
+  
+}
 
 
 
